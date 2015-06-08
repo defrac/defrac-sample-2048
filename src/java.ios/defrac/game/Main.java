f3ad0dae513a7f8178ecd6f8d5ab84f4d729504e
Joa Ebert <joa.ebert@defrac.com>
2015-06-08 11:19:32 +0200

    Simplify bootstrapping for iOS

--- src/java.ios/defrac/game/Main.java
+++ src/java.ios/defrac/game/Main.java
@@ -1,9 +1,19 @@
 package defrac.game;
 
-/**
- *
- */
-public final class Main {
-  private Main() {
+import defrac.app.ScreenAppDelegate;
+import defrac.ios.uikit.UIKit;
+import defrac.ui.Screen;
+
+import javax.annotation.Nonnull;
+
+public final class Main extends ScreenAppDelegate {
+  public static void main(String[] args) {
+    UIKit.applicationMain(args, null, Main.class);
+  }
+
+  @Override
+  @Nonnull
+  public Screen createScreen() {
+    return new GameScreen();
   }
 }
